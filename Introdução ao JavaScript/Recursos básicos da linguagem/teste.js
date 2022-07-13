6b5c0e0eaacb5356c063dd8f1df6db9de523bf67
Henrique Souza Silva <101906352+Henrique2811@users.noreply.github.com>
2022-07-13 02:20:51 -0300

    Add files via upload

---
+++ Introdução ao JavaScript/Recursos básicos da linguagem/teste.js
@@ -0,0 +1,16 @@
+var preco = 2;
+var desconto = 0.2;
+const PRECO = 2; 
+var total = PRECO - desconto;
+
+
+function soma(a, b) {
+    console.log(a + b) //Apenas ver o valor no console para ter certeza de que aquilo está funcionando
+    return a + b; //Quando não precisa retornar e manipular o resultado de uma função não precisa utilizar o return
+}
+
+function soma(a, b) {
+    return a + b; // 802
+}
+
+soma(3, 5);
